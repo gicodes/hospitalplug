66f6c51b3af11cb0ab70beb638513b2d0007f20a
Gideon I <gicodes9@gmail.com>
2025-06-28 12:22:54 +0100

    Framework for dashboard/admin designed

---
+++ src/app/components/dashboard/admin/view.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import styles from '../../../dashboard/admin/page.module.css'
+
+const Views = () => {
+  return (
+    <div className={styles.pageView}>
+
+    </div>
+  )
+}
+
+export default Views
--- src/app/dashboard/admin/page.tsx
+++ src/app/dashboard/admin/page.tsx
@@ -1,8 +1,19 @@
-import React from 'react'
+import React from 'react';
+import styles from './page.module.css'
+import Header from '@/app/components/dashboard/admin/header';
+import Menu from '@/app/components/dashboard/admin/menu';
+import Views from '@/app/components/dashboard/admin/view';
 
 const AdminConsole = () => {
   return (
-    <div>AdminConsole</div>
+    <div className={styles.page}>
+      <Header />
+
+      <div className={styles.console}>
+        <Menu />
+        <Views/>
+      </div>   
+    </div>
   )
 }
 
